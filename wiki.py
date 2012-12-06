7147a7608a3a53e41713059df6af32371120893e
James Glenn <thedonkdonk@gmail.com>
2012-12-06 14:33:28 -0600

    Adding wiki.py

---
+++ wiki.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python2
+# Must use Python 2.
+# Import list of servers and services from Icinga and make pages for the Wiki.
+# Will overwrite existing pages.
+
+import json
+import urllib2
+import mwclient # See http://sourceforge.net/apps/mediawiki/mwclient/index.php?title=Main_Page
+
+# Edit these
+wikiusername = "admin"
+wikipassword = "admin123"
+icingausername = "admin"
+icingapassword = "admin123"
+icingaurl = "https://icinga.example.com/cgi-bin/icinga/status.cgi" # Needs to be the location of your status.cgi
+# I'm assuming you use HTTPS. You really should.
+wikihost = ('https','en.wikipedia.org')
+
+# Config for the Wiki
+site = mwclient.Site(wikihost,path='/w/') # You might need to change the path, but this is the default.
+site.login(wikiusername, wikipassword)
+
+# This gets through the HTTP Basic Auth in Icinga
+passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+passman.add_password(None, icingaurl+"?style=hostdetail&jsonoutput", icingausername, icingapassword)
+urllib2.install_opener(urllib2.build_opener(urllib2.HTTPBasicAuthHandler(passman)))
+# This is the list of hosts.
+req = urllib2.Request(icingaurl+"?style=hostdetail&jsonoutput")
+data = urllib2.urlopen(req)
+json_data = data.read()
+j = json.loads(json_data)
+
+# Start a counter for looping through the json listing of hosts
+x = int(0)
+
+for i in j['status']['host_status']:
+    hostname = j['status']['host_status'][x]['host']
+    passman2 = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    passman2.add_password(None, icingaurl + "?host=" + hostname + "&jsonoutput", icingausername, icingapassword)
+    urllib2.install_opener(urllib2.build_opener(urllib2.HTTPBasicAuthHandler(passman)))
+    # This is the lists of services on each host.
+    req2 = urllib2.Request(icingaurl + "?host=" + hostname + "&jsonoutput")
+    data2 = urllib2.urlopen(req2)
+    json_data2 = data2.read()
+    j2 = json.loads(json_data2)
+    # Start a counter for looping through the json listing of services on each host
+    y = int(0)
+    # Start building text for the Page.
+    page = site.Pages[hostname]
+    pagetext = "== Services ==\r\n\r\n"
+    for i in j2['status']['service_status']:
+        pagetext += "=== "+ j2['status']['service_status'][y]['service'] + " ===\r\n\r\n"
+        y += 1
+    pagetext += "[[Category:Icinga]]\r\n\r\n"
+    # Save the pagetext.
+    page.save(pagetext, summary='Creating new page')
+    x += 1
+print "Done"
