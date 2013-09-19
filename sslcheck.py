547bb0c7cc31d351779e3ef8d8f7d165bbb79c89
James Glenn <thedonkdonk@gmail.com>
2013-09-18 22:14:48 -0500

    Actually useful

--- sslcheck.py
+++ sslcheck.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os
 import sys
 import argparse
 import socket
-import json
 import ssl
-import pprint
 from backports.ssl_match_hostname import match_hostname, CertificateError
 
 parser = argparse.ArgumentParser(description='SSL Certificate Report')
-
-parser.add_argument('-H', '--hostname', type=str, help='Host Name', required=True)
+parser.add_argument('-H', '--hostname', type=str, help='Host Name', required=Tru
 
 args = parser.parse_args()
-
-
 hostname = args.hostname
-
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-ssl_sock = ssl.wrap_socket(sock, ssl_version=ssl.PROTOCOL_SSLv3, cert_reqs=ssl.CERT_REQUIRED, ca_certs='/etc/pki/tls/cert.pem')
+ssl_sock = ssl.wrap_socket(sock, ssl_version=ssl.PROTOCOL_SSLv3, cert_reqs=ssl.C
 
 try:
     ssl_sock.connect((hostname, 443))
@@ -37,28 +29,7 @@ except ValueError:
     sys.exit(1)
 
 ciphertype = ssl_sock.cipher()[1]
-bitsize = int(ssl_sock.cipher()[2]) * 8
-
-data = ssl_sock.getpeercert()['subject']
 expires = ssl_sock.getpeercert()['notAfter']
-
-print hostname
-print ciphertype
-print bitsize
-print pprint.pformat(ssl_sock.getpeercert())
-
-
-
-#country = data[0][0][1]
-#state = data[1][0][1]
-#city = data[2][0][1]
-#organization = data[3][0][1]
-#commonname = data[5][0][1]
-
-
-#print "%s, %s, %s, %s, %s" % (hostname, bitsize, ciphertype, commonname, expires)
-
-
 
 from M2Crypto import SSL, RSA
 
@@ -68,14 +39,15 @@ conn = SSL.Connection(ctx)
 conn.connect((hostname, 443))
 cert = conn.get_peer_cert()
 
-print cert.get_issuer().as_text()
-
-print cert.get_subject().as_text()
+certinfo = cert.get_subject().as_text()
 
 def size(self):
     """
     Return the size of the key in bytes.
     """
     return m2.pkey_size(self.pkey)
-print cert.get_pubkey().size()*8
+
+certsize = cert.get_pubkey().size()*8
+
+print "'%s', '%s', '%s', '%s', '%s'" % (hostname, ciphertype, certsize, certinfo
 
