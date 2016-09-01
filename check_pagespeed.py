6a5e0b37ed3dea56fc9267b3b7bcb24fd3bc9a59
James Glenn <thedonkdonk@gmail.com>
2016-09-01 14:55:04 -0500

    Final

--- check_pagespeed.py
+++ check_pagespeed.py
@@ -17,9 +17,9 @@ buffer = BytesIO()
 
 parser = argparse.ArgumentParser(description='Check Page Speed')
 parser.add_argument('-u', '--url', type=str, help='Full URL for page to check.', required=True)
-parser.add_argument('-w', '--warn', type=str, help='Total time to result in warning status in seconds.', required=True)
-parser.add_argument('-c', '--crit', type=str, help='Total time to result in critical status in seconds.', required=True)
-
+parser.add_argument('-w', '--warn', type=float, help='Total time to result in warning status in seconds.', required=False, default=15)
+parser.add_argument('-c', '--crit', type=float, help='Total time to result in critical status in seconds.', required=False, default=30)
+parser.add_argument('-t', '--timeout', type=int, help='Time out in seconds.', required=False, default=60)
 
 args = parser.parse_args()
 
@@ -29,34 +29,34 @@ if os.name == 'nt':
     c.setopt(c.SSL_VERIFYPEER, False);
 
 c.setopt(c.URL, args.url)
-c.setopt(c.WRITEDATA, buffer)
+c.setopt(c.WRITEFUNCTION, buffer.write)
 
-c.setopt(c.TIMEOUT, 60)
+c.setopt(c.TIMEOUT, args.timeout)
 c.setopt(c.VERBOSE, False)
-
 c.setopt(c.HEADER, True);
 c.setopt(c.ENCODING, "gzip");
 c.setopt(c.FOLLOWLOCATION, False);
 c.setopt(c.USERAGENT, "Page Speed Checker v1.0");
 
-c.perform()
+try:
+    c.perform()
+except:
+    print('UNKNOWN Error')
+    exitcode = 3
+    c.close()
+    sys.exit(exitcode)
+exitcode = 0
+if exitcode == 0:
+    if c.getinfo(c.TOTAL_TIME) > args.crit:
+        exittext = 'CRITICAL'
+        exitcode = 2
+    elif c.getinfo(c.TOTAL_TIME) > args.warn:
+        exittext = 'WARNING'
+        exitcode = 1
+    else:
+        exittext = 'OK'
+        exitcode = 0
+print('%s - Response Code: %d - Page Size: %d bytes - Speed: %d bytes/second | dns=%fs;;;0.000 connect=%fs;;;0.000 appconnect=%fs;;;0.000 pretransfer=%fs;;;0.000 start=%fs;;;0.000 total=%fs;%f;%f;0.000' % (exittext, c.getinfo(c.RESPONSE_CODE), c.getinfo(c.SIZE_DOWNLOAD), c.getinfo(c.SPEED_DOWNLOAD), c.getinfo(c.NAMELOOKUP_TIME), c.getinfo(c.CONNECT_TIME), c.getinfo(c.APPCONNECT_TIME), c.getinfo(c.PRETRANSFER_TIME), c.getinfo(c.STARTTRANSFER_TIME), c.getinfo(c.TOTAL_TIME), args.warn, args.crit))
 
-print('RESPONSE_CODE: %d' % c.getinfo(c.RESPONSE_CODE))
-print('SIZE_DOWNLOAD: %d bytes.' % c.getinfo(c.SIZE_DOWNLOAD))
-print('SPEED_DOWNLOAD: %d bytes/second.' % c.getinfo(c.SPEED_DOWNLOAD))
-
-if c.getinfo(c.TOTAL_TIME) > args.warn:
-    exittext = 'WARNING'
-    exitcode = 1
-elif c.getinfo(c.TOTAL_TIME) > args.crit:
-    exittext = 'CRITICAL'
-    exitcode = 2
-else:
-    exittext = 'OK'
-    exitcode = 0
-
-print('%s | dns=%fs;;;0.000 connect=%fs;;;0.000 appconnect=%fs;;;0.000 pretransfer=%fs;;;0.000 start=%fs;;;0.000 total=%fs;;;0.000' % (exittext, c.getinfo(c.NAMELOOKUP_TIME), c.getinfo(c.CONNECT_TIME), c.getinfo(c.APPCONNECT_TIME), c.getinfo(c.PRETRANSFER_TIME), c.getinfo(c.STARTTRANSFER_TIME), c.getinfo(c.TOTAL_TIME)))
-
-# getinfo must be called before close.
 c.close()
-
+sys.exit(exitcode)
